4fee553250b4eb06b73acce2ead2ff468f70efb6
Your Name <you@example.com>
2025-04-24 09:37:22 +0700

    replace chrome with firefox

--- src/test/java/org/example/TugasTest.java
+++ src/test/java/org/example/TugasTest.java
@@ -5,21 +5,22 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TugasTest {
-    static WebDriver chrome = new ChromeDriver();
+    static WebDriver firefox = new FirefoxDriver();
     static WebDriver edge = new EdgeDriver();
 
     @Test
-    public void testChrome() {
-        chrome.get("https://tedi.sv.ugm.ac.id");
+    public void testFirefox() {
+        firefox.get("https://tedi.sv.ugm.ac.id");
 
-        String title = chrome.getTitle();
+        String title = firefox.getTitle();
         assertEquals(title, "Website Departemen Teknik Elektro dan Informatika (TEDI), Sekolah Vokasi, UGM – TEDI SV UGM");
 
-        String currentUrl = chrome.getCurrentUrl();
+        String currentUrl = firefox.getCurrentUrl();
         assertEquals(currentUrl, "https://tedi.sv.ugm.ac.id/id/muka/");
     }
 
@@ -36,7 +37,7 @@ public class TugasTest {
 
     @AfterAll
     public static void clear() {
-        chrome.quit();
+        firefox.quit();
         edge.quit();
     }
 }

54b8c6441eb36bc382b8f91bc14bcccf7413ea9c
Your Name <you@example.com>
2025-04-24 09:38:46 +0700

    remove unused import

--- src/test/java/org/example/TugasTest.java
+++ src/test/java/org/example/TugasTest.java
@@ -3,7 +3,6 @@ package org.example;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
